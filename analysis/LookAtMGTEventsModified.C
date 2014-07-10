f22ae9cc535e3d3e606b6407a4c3949a3227c80a
CarlyWilson <Carly.Jo.Wilson2011@gmail.com>
2014-07-10 12:35:42 -0500

    updated LookAtMGTEventsModified.C

--- analysis/LookAtMGTEventsModified.C
+++ analysis/LookAtMGTEventsModified.C
@@ -10,9 +10,9 @@
 	for(size_t i=startrun;i<endrun+1;i++)
 	{
 		sprintf(infile, "OR_run%d", i);
-    		sprintf(infilename, "/home/neutrino/mjd/ortec/data/built/P42661B/%s.root", infile);
-    		cout<<"added "<<infilename<<endl;
-    		t->AddFile(infilename);
+   		sprintf(infilename, "/home/neutrino/mjd/ortec/data/built/P42661B/%s.root", infile);
+   		cout<<"added "<<infilename<<endl;
+   		t->AddFile(infilename);
 	}
 
 	MGTEvent* event = new MGTEvent();
@@ -40,20 +40,20 @@
 	size_t nentries=t->GetEntries();
   
 	TCanvas c1;
-	size_t next=1, maxBin, T50Bin;
+	size_t next = 1, maxBin, T50Bin;
 	double energy=0.0, filterenergy, maxHeight, norm = 1;
 	vector<double> v100(100);
 	char title[100];
 
 	size_t n = 10;
 	TH1F** h1 = new TH1F* [n];
-	TH1F h100("h100", "", 100, 0, 100);
+	TH1F h100("h100", "", -100, 0, 100);
 
 	for(size_t i = 0; i < n; i++)
 	{
 		t->GetEntry(i);
 
-			if(event->GetWaveforms()<1)continue;
+		if(event->GetWaveforms()<1)continue;
 
     	Wave=event->GetWaveform(0);
     	base->TransformInPlace(*Wave);
@@ -74,42 +74,47 @@
 		h1[i]->GetYaxis()->SetRangeUser(TMath::Min(h1[i]->GetMinimum(), 0.0), 1.05*TMath::Max(h1[i]->GetMaximum(), 0.0));
 		h1[i]->SetTitle(title);
 
-		h1[i]->SetLineWidth(3);
+		//h1[i]->SetLineWidth(3);
+		h100->SetLineWidth(3);
 
-			if(i > 0 && i < 20) h1[i]->SetLineColor(i + 1);
-			else if(i >= 20 && i > 0) h1[i]->SetLineColor(i - 1);
-			else h1[i]->SetLineColor(1);
-		
+		//h1[i]->SetLineColor(i % 9 + 1);
+		h100->SetLineColor(i % 9 + 1);
+				
 		h1[i]->Scale(norm/h1[i]->Integral());
+		
+		//if(i == 0) h1[i]->Draw();
+		//else h1[i]->Draw("same");
 
-			if(i == 0) h1[i]->Draw();
-			else h1[i]->Draw("same");
+		maxHeight = h1[i]->GetMaximum();
+  		maxBin = h1[i]->GetMaximumBin();
+  		T50Bin = maxBin;
 
-		maxHeight=h1[i]->GetMaximum();
-  		maxBin=h1[i]->GetMaximumBin();
-  		T50Bin=maxBin;
-
-  			while(h1[T50Bin] > 0.5 * maxHeight && maxBin > 0)
-			{
-   				maxBin--;
-  			}
+  		while(h1[T50Bin] > 0.5 * maxHeight && maxBin > 0)
+		{
+   			maxBin--;
+  		}
 
   		cout<<"The maximum is "<<maxHeight<<" at "<<h1[i]->GetBinCenter(maxBin)<<" T50 is at "<<h1[i]->GetBinCenter(T50Bin)<<endl;
+		cout<<"T50Bin: "<<T50Bin<<endl;
+		cout<<"maxBin: "<<maxBin<<endl;
+		for(size_t j = 1; j < 101; j++);
+		{
+			h100->SetBinContent(j, h1[i]->GetBinContent(T50Bin - 50 + j));
+			v100[j] = h1[i]->GetBinContent(T50Bin - 50 + j);
+			h100->Fill(v100[j]);
+		}
 
-			for(size_t j = 1; j < 101; j++);
-			{
-				h100->SetBinContent(j, h1[i]->GetBinContent(T50Bin - 50 + j));
-				v100[i] = h1[i]->GetBinContent(T50Bin - 50 + j);
-			}
+		h100->Scale(norm/h100->Integral());
 
-		//h100->Draw();//be careful if T50Bin<50 or > hwave->GetNbinsX()
+		if(i == 0) h100->Draw();//be careful if T50Bin<50 or > hwave->GetNbinsX()
+		else h100->Draw("same");
 
         cout<<"energy "<<energy<<" filter energy "<<filterenergy<<endl;
 		
 		c1.Update();
 
         cin>>next;
-			if(next<1)break;
+		if(next<1)break;
         }
 }
 
